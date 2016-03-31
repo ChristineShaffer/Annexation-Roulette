98b03db9c98a11ec0f1564ad3f785866197eb9b7
ChristineShaffer <cshaffer@ualberta.ca>
2016-03-31 14:31:27 -0600

    First Commit

---
+++ public_html/js/index.js
@@ -0,0 +1,18 @@
+$("document").ready( function() {   
+    
+    $("#button").click( function() {
+        
+        var mapPartArray = ["#map-part1", "#map-part2", "#map-part3"];
+        
+        // Get random number
+        
+        // Highlight each area for X number of seconds, incrementally getting 
+        // longer and longer until it stops on an area
+        
+        // Once stopped on area flash it then leave highlighted
+        
+        $("#map-part1").toggleClass("hovered");
+        
+    });
+    
+});
