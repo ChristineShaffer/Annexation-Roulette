6cf95d76d9c932fb6126223629784638eb9b0fda
ChristineShaffer <cshaffer@ualberta.ca>
2016-04-01 08:46:56 -0600

    Got random function working; finished all of highlighted areas I think

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -3,16 +3,30 @@ $("document").ready( function() {
     $("#button").click( function(e) {
         e.preventDefault();
         
-        var mapPartArray = ["#map-part1", "#map-part2", "#map-part3"];
+        var mapPartArray = ["#map-part1", "#map-part2", "#map-part3", 
+                            "#map-part4", "#map-part5", "#map-part6", 
+                            "#map-part7"];
         
         // Get random number
+        var rand = Math.random() + 1000;
         
         // Highlight each area for X number of seconds, incrementally getting 
         // longer and longer until it stops on an area
+        var i = 0;
+        var n = mapPartArray.length;
+        function highlight() {
+            if(i < n) {
+                console.log(rand);
+                var curPart = mapPartArray[i];
+                $(curPart).toggleClass("hovered");
+                rand += 100;
+                i++;
+                setTimeout( highlight, rand);
+            }
+        }
+        setTimeout(highlight, 0);
         
         // Once stopped on area flash it then leave highlighted
-        
-        $("#map-part1").toggleClass("hovered");
         
     });
     
